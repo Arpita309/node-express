e51b77f1704f5ee44ca2f57b440ed09f623ba73a
arpita tiwari <arpitatiwari309@gmail.com>
2020-03-11 14:38:18 +0530

    Express Router

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express'),
-    http = require('http');
-const morgan = require('morgan');
+     http = require('http');
+     const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
-
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 const app = express();
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
-    app.use(morgan('dev'));
+   
 
-    app.use(express.static(__dirname + '/public'));
+
+ 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
